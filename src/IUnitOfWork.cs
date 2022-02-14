cb587eaae003dac2b86d1c1ae13e49a7cf9f47a7
Nelson Nobre <technobre@gmail.com>
2022-02-14 01:01:52 +0000

    Kickoff

---
+++ src/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PowerUtils.BuildingBlocks.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<bool> CommitAsync(CancellationToken cancellationToken = default);
+        Task RollbackAsync(CancellationToken cancellationToken = default);
+    }
+}
