b3e7b2ca1bf1bb4c8ef09e2020015497936c1e13
Nelson Nobre <technobre@gmail.com>
2024-03-03 01:18:24 +0000

    fix: Marked all implementations as `Obsolete`
    BREAKING CHANGE: Deprecated package

--- src/IUnitOfWork.cs
+++ src/IUnitOfWork.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 
 namespace PowerUtils.BuildingBlocks.Data
 {
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public interface IUnitOfWork : IDisposable
     {
         Task<bool> CommitAsync(CancellationToken cancellationToken = default);
--- src/Repositories/IRepositoryBase.cs
+++ src/Repositories/IRepositoryBase.cs
@@ -1,4 +1,7 @@
-﻿namespace PowerUtils.BuildingBlocks.Data.Repositories
+﻿using System;
+
+namespace PowerUtils.BuildingBlocks.Data.Repositories
 {
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public interface IRepositoryBase { }
 }
